0b7cdebc296c80aa1d268b76d66cc39bf0ee291a
Dennis Jojo Kuriakose <62955521+atmozki@users.noreply.github.com>
2022-09-25 23:07:56 +0530

    Edited Footer

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,14 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-remark-emoji`,  // <-- this line adds emoji
+        ]
+      }
+    },
     'gatsby-plugin-sass',
   ],
 }
